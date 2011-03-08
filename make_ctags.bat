fd99093a4a14c3beaeccaf5a640aa53c5022e38c
sasami <sasami@samynet.ddo.jp>
2011-03-08 21:39:06 +0900

    make_ctags.bat: io*.hをiom328p.h iom168p.h iotn2313.hに限定した。

--- make_ctags.bat
+++ make_ctags.bat
@@ -1,1 +1,14 @@
-c:\wintools\ctags\ctags -R "C:\Program Files\WinAVR-20100110\avr\include" samylib
+if "X%CTAGS_CMD%" == "X" (
+	set CTAGS_CMD=C:\wintools\ctags\ctags
+)
+if "X%WINAVR_INCLUDE%" == "X" (
+	set WINAVR_INCLUDE=C:\Program Files\WinAVR-20100110\avr\include
+)
+
+"%CTAGS_CMD%" -R samylib
+dir /B "%WINAVR_INCLUDE%\avr\io*.h" > ctags_exclude_files.txt
+"%CTAGS_CMD%" -a -R --exclude=@ctags_exclude_files.txt "%WINAVR_INCLUDE%"
+"%CTAGS_CMD%" -a "%WINAVR_INCLUDE%\avr\iom328p.h"
+"%CTAGS_CMD%" -a "%WINAVR_INCLUDE%\avr\iom168p.h"
+"%CTAGS_CMD%" -a "%WINAVR_INCLUDE%\avr\iotn2313.h"
+"%CTAGS_CMD%" -a "%WINAVR_INCLUDE%\avr\io.h"
