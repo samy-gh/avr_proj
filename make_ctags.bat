ba2556dc24fe9c7eb289659a0f217afd201e3c53
sasami <sasami@samynet.ddo.jp>
2011-03-17 06:49:19 +0900

    make_ctags.bat: ctagsオプションを変更。

--- make_ctags.bat
+++ make_ctags.bat
@@ -5,10 +5,10 @@ if "X%WINAVR_INCLUDE%" == "X" (
 	set WINAVR_INCLUDE=C:\Program Files\WinAVR-20100110\avr\include
 )
 
-"%CTAGS_CMD%" -R samylib
+"%CTAGS_CMD%" -R --tag-relative=yes samylib
 dir /B "%WINAVR_INCLUDE%\avr\io*.h" > ctags_exclude_files.txt
-"%CTAGS_CMD%" -a -R --exclude=@ctags_exclude_files.txt "%WINAVR_INCLUDE%"
-"%CTAGS_CMD%" -a "%WINAVR_INCLUDE%\avr\iom328p.h"
-"%CTAGS_CMD%" -a "%WINAVR_INCLUDE%\avr\iom168p.h"
-"%CTAGS_CMD%" -a "%WINAVR_INCLUDE%\avr\iotn2313.h"
-"%CTAGS_CMD%" -a "%WINAVR_INCLUDE%\avr\io.h"
+"%CTAGS_CMD%" -a -R --tag-relative=yes --exclude=@ctags_exclude_files.txt "%WINAVR_INCLUDE%"
+"%CTAGS_CMD%" -a --tag-relative=yes "%WINAVR_INCLUDE%\avr\iom328p.h"
+"%CTAGS_CMD%" -a --tag-relative=yes "%WINAVR_INCLUDE%\avr\iom168p.h"
+"%CTAGS_CMD%" -a --tag-relative=yes "%WINAVR_INCLUDE%\avr\iotn2313.h"
+"%CTAGS_CMD%" -a --tag-relative=yes "%WINAVR_INCLUDE%\avr\io.h"
