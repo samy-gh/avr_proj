55605d6b2208c0b52d3eab25c2492c8cd7ebd3a9
sasami <sasami@samynet.ddo.jp>
2011-03-20 13:10:17 +0900

    samylib/lcd: Lcd_Set_Block()にてblockを複数回実施できるようにした。

--- samylib/lcd/lcd_set_block.c
+++ samylib/lcd/lcd_set_block.c
@@ -10,16 +10,21 @@
 #include "int_lock.h"
 
 
+static UCHAR glcd_block_count = 0;
+
 BOOL Lcd_Set_Block( BOOL block_stat )
 {
 	UCHAR const lockKey = Int_Lock();
 
 	if( block_stat ) {
-		if( gLcd_Is_Open != E_LCD_IS_OPEN_FREE ) {
+		if( gLcd_Is_Open == E_LCD_IS_OPEN_USING ) {
 			Int_Unlock( lockKey );
 			return FALSE;
 		}
 
+		if( glcd_block_count != 255 ) {
+			glcd_block_count++;
+		}
 		gLcd_Is_Open = E_LCD_IS_OPEN_BLOCK;
 	}
 	else {
@@ -28,7 +33,12 @@ BOOL Lcd_Set_Block( BOOL block_stat )
 			return FALSE;
 		}
 
-		gLcd_Is_Open = E_LCD_IS_OPEN_FREE;
+		if( glcd_block_count != 0 ) {
+			glcd_block_count--;
+			if( glcd_block_count == 0 ) {
+				gLcd_Is_Open = E_LCD_IS_OPEN_FREE;
+			}
+		}
 	}
 	Int_Unlock( lockKey );
 
