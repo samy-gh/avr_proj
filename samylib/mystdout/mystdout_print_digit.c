39431a1a8ab910aeb0a3dcc1687f0085f71b3223
sasami <sasami@samynet.ddo.jp>
2012-04-01 11:13:15 +0900

    Mystdout_PrintDigit()にて0を出力できない問題を修正。

--- samylib/mystdout/mystdout_print_digit.c
+++ samylib/mystdout/mystdout_print_digit.c
@@ -5,10 +5,17 @@
 // samylib
 #include "my_typedef.h"
 
+// STD-C
+#include <stdio.h>
+
 
 VOID Mystdout_PrintDigit( UINT val )
 {
 	UINT print_lvl;
+
+	if( val == 0 ) {
+		stdout->put( '0', NULL );
+	}
 
 	// 最上位桁までスキップ
 	for( print_lvl = 10000; print_lvl != 0; print_lvl /= 10 ) {
