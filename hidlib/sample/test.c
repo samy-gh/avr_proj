770aa7fb0c7af82ff92fa4587d963122478ae807
sasami <sasami@samynet.ddo.jp>
2012-07-16 15:54:56 +0900

    hidlib/sample: ReportReadのサンプルを実装。

--- hidlib/sample/test.c
+++ hidlib/sample/test.c
@@ -7,10 +7,21 @@
 #define	MY_Manufacturer	"SAMYNET"
 #define	MY_Product		"HIDtest"
 
+void dump_buf( unsigned char* buf, unsigned int len )
+{
+	unsigned int loopcnt;
+
+	for( loopcnt = 0; loopcnt < len; loopcnt++ ) {
+		printf( " %02x", buf[loopcnt] );
+	}
+	printf( "\n" );
+}
+
 int main( int argc, char* argv[] )
 {
 	const T_HID_HDL* hid_hdl;
 	char buf[40];
+	int  result;
 
 	HidInit();
 
@@ -31,7 +42,20 @@ int main( int argc, char* argv[] )
 	buf[7] = 17;
 	ReportWrite( hid_hdl, buf, 7 );
 	ReportWrite( hid_hdl, buf, 8 );
-	
+
+	memset( buf, '\0', sizeof(buf) );
+	result = ReportRead( hid_hdl, buf, 7, 1 );
+	printf( "ReportRead result = %d\n", result );
+	if( result == 1 ) {
+		dump_buf( buf, 7 );
+	}
+	memset( buf, '\0', sizeof(buf) );
+	result = ReportRead( hid_hdl, buf, 31, 2 );
+	printf( "ReportRead result = %d\n", result );
+	if( result == 1 ) {
+		dump_buf( buf, 31 );
+	}
+
 	HidClose( hid_hdl );
 
 	HidFree();
